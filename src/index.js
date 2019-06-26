454f081bcb922990c95e76bd98b27f924b16d115
Paulo José de Oliveira Salgado <paulo@technosoftware.com.br>
2019-06-26 18:20:12 -0300

    Projeto finalizado.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as Sentry from '@sentry/browser';
+
+import './global.css'
+
+Sentry.init({
+    dsn: "https://c23cc3e77eac433ea43731ab99e6cba9@sentry.io/1491201", 
+    environment: "dev"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

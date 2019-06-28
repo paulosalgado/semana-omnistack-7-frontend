7effcf91a15897ac1c94c299349dcdd3e8b4d0dd
Paulo José de Oliveira Salgado <paulo@technosoftware.com.br>
2019-06-28 11:29:56 -0300

    Alterado endpoint de serviços.

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -25,7 +25,7 @@ class Feed extends Component {
 
     registerToSocket = () => {
 
-        const socket = io('http://localhost:3333');
+        const socket = io('https://backend-semana-omnistack-7.herokuapp.com');
     
         socket.on('post', newPost => {
             this.setState({ feed: [newPost, ...this.state.feed] });
@@ -56,7 +56,7 @@ class Feed extends Component {
                             </div>
                             <img src={more} alt="Mais" />
                         </header>
-                        <img src={`http://localhost:3333/files/${post.image}`} alt="" />
+                        <img src={`https://backend-semana-omnistack-7.herokuapp.com/files/${post.image}`} alt="" />
                         <footer>
                             <div className="actions">
                                 <button type="button" onClick={() => this.handleLike(post._id)}>
